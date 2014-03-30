bfc24751837975862c00ea67adf11c87e2f3f1a9
AragaoAnderson <andersonaragao@me.com>
2014-03-30 00:00:21 -0300

    Settings: Quiet Hours Automatic Switch [2/2]
    Change-Id: Ic87531b6b7a4cd5925c5406701fd681c04465db6

--- src/com/android/settings/cyanogenmod/QuietHours.java
+++ src/com/android/settings/cyanogenmod/QuietHours.java
@@ -19,6 +19,10 @@ package com.android.settings.cyanogenmod;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.ListPreference;
@@ -33,9 +37,11 @@ public class QuietHours extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener  {
 
     private static final String TAG = "QuietHours";
+    private static final String KEY_QUIET_HOURS_AUTO = "quiet_hours_auto";
     private static final String KEY_QUIET_HOURS_TIMERANGE = "quiet_hours_timerange";
     private static final CharSequence KEY_QUIET_HOURS_RINGER = "quiet_hours_ringer";
 
+    private ListPreference mAutoEnable;
     private TimeRangePreference mQuietHoursTimeRange;
     private ListPreference mQuietHoursRinger;
 
@@ -46,8 +52,11 @@ public class QuietHours extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.quiet_hours_settings);
         Resources res = getResources();
         ContentResolver resolver = getContentResolver();
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         // Load the preferences
+        mAutoEnable =
+                (ListPreference) findPreference(KEY_QUIET_HOURS_AUTO);
         mQuietHoursTimeRange =
                 (TimeRangePreference) findPreference(KEY_QUIET_HOURS_TIMERANGE);
         mQuietHoursTimeRange.setTimeRange(
@@ -55,19 +64,26 @@ public class QuietHours extends SettingsPreferenceFragment implements
                 Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_END, 0));
         mQuietHoursTimeRange.setOnPreferenceChangeListener(this);
 
+        mAutoEnable.setValue(mPrefs.getString(KEY_QUIET_HOURS_AUTO, "0"));
+        mAutoEnable.setOnPreferenceChangeListener(this);
+
         // Remove the ringer setting on non-telephony devices else enable it
         mQuietHoursRinger = (ListPreference) findPreference(KEY_QUIET_HOURS_RINGER);
         TelephonyManager telephonyManager =
                 (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         if (telephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE) {
+            getPreferenceScreen().removePreference(mAutoEnable);
             getPreferenceScreen().removePreference(mQuietHoursRinger);
             mQuietHoursRinger = null;
         } else {
+            int autoMode = Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_AUTOMATIC, 0);
             int muteType = Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_RINGER, 0);
             mQuietHoursRinger.setValue(String.valueOf(muteType));
             mQuietHoursRinger.setSummary(mQuietHoursRinger.getEntry());
             mQuietHoursRinger.setOnPreferenceChangeListener(this);
         }
+
+        mAutoEnable.setSummary(mAutoEnable.getEntries()[autoMode]);
 
         // Remove the notification light setting if the device does not support it
         if (!res.getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed)) {
@@ -85,7 +101,29 @@ public class QuietHours extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getContentResolver();
-        if (preference == mQuietHoursTimeRange) {
+        if (preference == mAutoEnable) {
+            int val = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_AUTOMATIC,
+                    val);
+            if (val != 0) {
+                AudioManager audioManager =
+                        (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+                final int ringerMode = audioManager.getRingerMode();
+                boolean enabled = false;
+                if (ringerMode == AudioManager.RINGER_MODE_SILENT) {
+                    enabled = true;
+                } else if (ringerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                    if (val == 2) {
+                        enabled = true;
+                    }
+                }
+                Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_ENABLED,
+                        enabled ? 1 : 0);
+                mQuietHoursEnabled.setChecked(enabled);
+            }
+            mAutoEnable.setSummary(mAutoEnable.getEntries()[val]);
+            return true;
+        } else if (preference == mQuietHoursTimeRange) {
             Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_START,
                     mQuietHoursTimeRange.getStartTime());
             Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_END,
