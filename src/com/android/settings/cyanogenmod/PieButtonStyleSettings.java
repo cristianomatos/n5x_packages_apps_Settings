3590b3a7a0561ea390b005b447d1c1f8cd332c6a
AragaoAnderson <andersonaragao@me.com>
2014-05-03 13:44:48 -0300

    Settings: Fix SlimPie implementation
    Change-Id: Ifa515649ebe15fc85cdcaa0b71a7e46282d2b7a5

--- src/com/android/settings/cyanogenmod/PieButtonStyleSettings.java
+++ src/com/android/settings/cyanogenmod/PieButtonStyleSettings.java
@@ -36,6 +36,7 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
