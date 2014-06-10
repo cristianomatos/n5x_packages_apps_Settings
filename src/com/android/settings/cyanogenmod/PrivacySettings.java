5b17bffdf95fb7b0764c993388738c8ae22aaa55
AragaoAnderson <andersonaragao@me.com>
2014-06-10 11:27:46 -0300

    Settings: Remove CM stats from Privacy
    Change-Id: I0061878a2f65d07ded8c5ce4d30a8fac03b7e63e

--- src/com/android/settings/cyanogenmod/PrivacySettings.java
+++ src/com/android/settings/cyanogenmod/PrivacySettings.java
@@ -54,12 +54,12 @@ public class PrivacySettings extends SettingsPreferenceFragment {
             mBlacklist = null;
         }
 
-        addPreferencesFromResource(R.xml.security_settings_cyanogenmod);
+        //addPreferencesFromResource(R.xml.security_settings_cyanogenmod);
         // Logger
         // Only add if device has Logger installed
-        if (Utils.isPackageInstalled(getActivity(), "com.cyngn.logger")) {
-            addPreferencesFromResource(R.xml.security_settings_logger);
-        }
+        //if (Utils.isPackageInstalled(getActivity(), "com.cyngn.logger")) {
+        //    addPreferencesFromResource(R.xml.security_settings_logger);
+        //}
     }
 
     @Override
