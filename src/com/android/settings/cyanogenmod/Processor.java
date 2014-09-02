6e8a6b49f44d7dae03576ab08effbab0a51f4ae9
AragaoAnderson <andersonaragao@me.com>
2014-09-01 23:08:58 -0300

    Settings: Fix derps after remove Power Saver
    Change-Id: I66146643bf2a14c269370ea07654df0a19025476

--- src/com/android/settings/cyanogenmod/Processor.java
+++ src/com/android/settings/cyanogenmod/Processor.java
@@ -16,18 +16,14 @@
 
 package com.android.settings.cyanogenmod;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.os.SystemService;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
-import android.provider.Settings;
 import android.util.Log;
 
 import com.android.settings.R;
@@ -64,8 +60,6 @@ public class Processor extends SettingsPreferenceFragment implements
     private String mGovernorFormat;
     private String mMinFrequencyFormat;
     private String mMaxFrequencyFormat;
-
-    private PowerManager mPowerManager;
 
     private Preference mCurFrequencyPref;
     private ListPreference mGovernorPref;
@@ -113,8 +107,6 @@ public class Processor extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
         initFreqCapFiles();
 
@@ -327,7 +319,7 @@ public class Processor extends SettingsPreferenceFragment implements
                 }
 
                 if (preference == mGovernorPref) {
-                    mGovernorPref.setSummary(String.format(mGovernorFormat, (String) newValue));
+                    mGovernorPref.setSummary(String.format(mGovernorFormat, newValue));
                 } else if (preference == mMinFrequencyPref) {
                     mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat,
                             toMHz(newValue)));
